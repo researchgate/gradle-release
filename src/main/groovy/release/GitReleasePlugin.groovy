3fe6fe5b516a56a784a07803a4cfa3b520a3228c
Evgeny Goldin <evgenyg@gmail.com>
2011-09-20 12:36:57 +0300

    GitReleasePlugin - checks branch to be "master"

--- src/main/groovy/release/GitReleasePlugin.groovy
+++ src/main/groovy/release/GitReleasePlugin.groovy
@@ -9,44 +9,60 @@ import org.gradle.api.GradleException
  */
 class GitReleasePlugin extends BaseScmPlugin {
 
-    private static final String LINE              = '~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~'
+    private static final String LINE              = '~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~'
     private static final String NOTHING_TO_COMMIT = 'nothing to commit (working directory clean)'
 
 
+    private GitReleasePluginConvention gitConvention(){ convention( 'GitReleasePlugin', GitReleasePluginConvention) }
+    private List<String>               gitStatus()    { exec( 'git', 'status' ).readLines() }
+
+
     void init () {
-        project.convention.plugins.GitReleasePlugin = new GitReleasePluginConvention()
+
+        project.convention.plugins[ 'GitReleasePlugin' ] = new GitReleasePluginConvention()
+
+        if ( gitConvention().requireBranch ) {
+
+            def branch = gitStatus()[ 0 ].trim().find( /^# On branch (\S+)$/ ){ it[ 1 ] }
+
+            if ( ! ( branch == gitConvention().requireBranch )) {
+                throw new GradleException( "Current Git branch is \"$branch\" and not \"${ gitConvention().requireBranch }\"." )
+            }
+        }
     }
 
 
     void checkCommitNeeded () {
 
-        def status = exec( 'git', 'status' ).readLines()
+        def status = gitStatus()
 
         if ( ! status.grep( NOTHING_TO_COMMIT )) {
-            throw new GradleException( [ 'You have uncommitted or unversioned files:',
-                                       LINE, *status, LINE ].join( '\n' ))
+            throw new GradleException(([ 'You have uncommitted or unversioned files:', LINE, *status, LINE ] as String[] ).
+                                      join( '\n' ))
         }
     }
 
 
     void checkUpdateNeeded () {
 
-        exec( 'git', 'remote', 'update' )
-        def status    = exec( 'git', 'status' ).readLines()
+        exec([ 'git', 'remote', 'update' ], 'aaaaaa', 'aaaaaa' )
+
+        def status    = gitStatus()
         def noUpdates = ( status.size() == 2 ) &&
                         ( status[ 0 ].startsWith( '# On branch ' )) &&
                         ( status[ 1 ] == NOTHING_TO_COMMIT )
 
         if ( ! noUpdates ) {
-            throw new GradleException( [ 'You have remote changes to pull or local changes to push',
-                                       LINE, *status, LINE ].join( '\n' ))
+            throw new GradleException(( [ 'You have remote changes to pull or local changes to push', LINE, *status, LINE ] as String[] ).
+                                      join( '\n' ))
         }
     }
 
 
     void createReleaseTag () {
-        exec([ 'git', 'tag', '-a',      project.properties.version, '-m', 'v' + project.properties.version ], 'aaaa', 'aaaaaa' )
-        exec([ 'git', 'push', 'origin', project.properties.version ], 'aaaa', 'aaaaaa' )
+        def version = project.version
+        exec([ 'git', 'tag', '-a',      version, '-m', 'v' + version ], "Duplicate tag [$version]", 'already exists' )
+        exec([ 'git', 'push', 'origin', version ], 'aaaa', 'aaaaaa' )
     }
 
 
