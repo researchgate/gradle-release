53e36e8796d274207a7267b2337c6270cdad590e
Jeff Storey <jeff.storey@nextcentury.com>
2013-09-17 12:54:00 -0400

    reverting change accidentally committed to apply.groovy

--- installation/apply.groovy
+++ installation/apply.groovy
@@ -4,9 +4,7 @@ buildscript {
 		maven { url "https://oss.sonatype.org/content/groups/public"}
 	}
 	dependencies {
-		//classpath 'com.github.townsfolk:gradle-release:1.2-SNAPSHOT'
-        classpath files('/home/HQ/jstorey/workspace/gradle-release/build/classes/main')
-        classpath files('/home/HQ/jstorey/workspace/gradle-release/build/resources/main')
+		classpath 'com.github.townsfolk:gradle-release:1.2-SNAPSHOT'
 	}
 }
 // Check to make sure release.ReleasePlugin isn't already added.
