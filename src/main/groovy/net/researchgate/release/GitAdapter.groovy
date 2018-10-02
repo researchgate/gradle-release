f7e244d4c1a95f0a880edc06b3faf2971b01c53c
Adrien Ferrand <adrien.ferrand@amadeus.com>
2018-10-02 16:25:05 +0200

    Fail the process if there is a conflict during any merge

--- src/main/groovy/net/researchgate/release/GitAdapter.groovy
+++ src/main/groovy/net/researchgate/release/GitAdapter.groovy
@@ -172,7 +172,7 @@ class GitAdapter extends BaseScmAdapter {
     private checkoutMerge(String fromBranch, String toBranch) {
         exec(['git', 'fetch'], directory: workingDirectory, errorPatterns: ['error: ', 'fatal: '])
         exec(['git', 'checkout', toBranch], directory: workingDirectory, errorPatterns: ['error: ', 'fatal: '])
-        exec(['git', 'merge', '--no-ff', '--no-commit', '--strategy=recursive', '-Xtheirs', fromBranch], directory: workingDirectory, errorPatterns: ['error: ', 'fatal: '])
+        exec(['git', 'merge', '--no-ff', '--no-commit', fromBranch], directory: workingDirectory, errorPatterns: ['error: ', 'fatal: ', 'CONFLICT'])
     }
 
     private boolean shouldPush() {
